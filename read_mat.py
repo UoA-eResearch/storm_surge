4c8a86863bd23a5487f12a42cbd0225f57dda9c0
Nick Young <nick.young@auckland.ac.nz>
2018-09-10 13:55:47 +1200

    start index performance optimisation

--- read_mat.py
+++ read_mat.py
@@ -37,8 +37,9 @@ if model == 'dates':
 
 model_name = os.path.splitext(os.path.basename(model))[0]
 
-def find_dt_offset(daterow):
-    for i, date in enumerate(dates):
+def find_dt_offset(daterow, startIndex = 0):
+    for i in range(startIndex, len(dates)):
+        date = dates[i]
         if date.year == daterow[0] and date.month == daterow[1] and date.day == daterow[2]:
             return i
     return None
@@ -87,24 +88,28 @@ else:
         exit(1)
     with open(model_name + '.csv', 'w') as f:
         f.write("x,y,z,height,model\n")
+        normalized_z = 0
         for z in range(zl):
             dt = model['historical_dates'][z]
-            normalised_z = find_dt_offset(dt)
+            normalised_z = find_dt_offset(dt, normalized_z)
             for x in range(xl):
                 for y in range(yl):
                     hheight = sshistorical[z][x * yl + y]
                     f.write("{},{},{},{},{}\n".format(x, y, normalised_z, hheight, 0))
             print("{}/{} done".format(z, zl))
         zl = rcp45.shape[0]
+        normalized_z = 0
+        rcp45z = 0
+        rcp85z = 0
         for z in range(zl):
             if 'future_dates' in model:
                 dt = model['future_dates'][z]
-                normalised_z = find_dt_offset(dt)
+                normalised_z = find_dt_offset(dt, normalized_z)
                 rcp45z = normalised_z
                 rcp85z = normalised_z
             else:
-                rcp45z = find_dt_offset(model['future_dates45'][z])
-                rcp85z = find_dt_offset(model['future_dates85'][z])
+                rcp45z = find_dt_offset(model['future_dates45'][z], rcp45z)
+                rcp85z = find_dt_offset(model['future_dates85'][z], rcp85z)
             for x in range(xl):
                 for y in range(yl):
                     rcp45height = rcp45[z][x * yl + y]
