eaab168917cd2cb3e8fe81cf75433a5456fc1d99
Nick Young <nick.young@auckland.ac.nz>
2018-09-11 16:30:23 +1200

    export to a zipped csv

--- web_server.py
+++ web_server.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
-from bottle import Bottle, request, response
+from bottle import Bottle, request, response, static_file
 import bottle_mysql
 import csv
 import os
 import sys
+from datetime import datetime
+import csv
+from zipfile import ZipFile
 
 application = Bottle()
 plugin = bottle_mysql.Plugin(dbuser='storm_ro', dbpass='storm', dbname='storm')
@@ -25,13 +28,32 @@ def get(db):
     lltable = "f_latlng"
     if model == 'Model_20CR':
         lltable = "latlng"
-    query = "SELECT ST_Y(l.latlng) AS lat, ST_X(l.latlng) AS lng, m.height, DATE_FORMAT(d.datetime, '%Y-%m-%d %H:%i:%s') AS dt FROM `" + model + "` m INNER JOIN `" + lltable + "` l ON m.x = l.x AND m.y = l.y INNER JOIN date d ON m.z = d.id "
+    query = "SELECT ST_Y(l.latlng) AS lat, ST_X(l.latlng) AS lng, m.height, DATE_FORMAT(d.datetime, '%Y-%m-%d %H:%i:%s') AS datetime FROM `" + model + "` m INNER JOIN `" + lltable + "` l ON m.x = l.x AND m.y = l.y INNER JOIN date d ON m.z = d.id "
     query += "WHERE MBRContains(ST_GeomFromText('" + bounds + "'), l.latlng) AND d.datetime BETWEEN '" + mindate + "' AND '" + maxdate + "';"
     print(query)
     db.execute(query)
     results = db.fetchall()
     print("{} results".format(len(results)))
-    return {"results": results}
+    response_format = request.params.get('format', 'json')
+    if response_format == 'csv':
+        dt = datetime.now().isoformat()
+        filename = "{}_export_{}.csv".format(model, dt)
+        filename_with_path = os.path.join("exports", filename)
+        with open(filename_with_path, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=["lat", "lng", "datetime", "height"])
+            writer.writeheader()
+            writer.writerows(results)
+        zipfilename = filename_with_path + ".zip"
+        with ZipFile(zipfilename, "w") as zip:
+            zip.write(filename_with_path, filename)
+        return zipfilename
+    else:
+        return {"results": results}
+
+@application.get('/exports/<filename>')
+def serve_export(filename):
+    print("request for " + filename)
+    return static_file(filename, root='exports')
 
 @application.get('/ranges')
 def get_ranges(db):
